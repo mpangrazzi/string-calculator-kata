76a144962d35e77d1fa311373b51e02071a27a61
xmikex83 <xmikex83@gmail.com>
2016-04-04 12:37:03 +0200

    support different delimiters; added test & examples

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 
-const SEPARATOR = /,|\n/;
+/**
+ * Module dependencies
+ */
+
+const debug = require('debug')('add');
+const DEFAULT_DELIMITERS = [',', '\n'];
+
 
 module.exports = function add (input) {
 
@@ -9,13 +15,9 @@ module.exports = function add (input) {
     return 0;
   }
 
-  // Split & parse
+  // Parse input
 
-  var numbers = input
-    .split(SEPARATOR)
-    .map(n => {
-      return parseInt(n);
-    });
+  var numbers = parse(input);
 
   // Sum (reduce)
 
@@ -24,3 +26,64 @@ module.exports = function add (input) {
   });
 
 };
+
+
+/**
+ * Parse an input string an return an array of Numbers
+ *
+ * @param  {string} input
+ * @return {Array}
+ */
+
+function parse (input) {
+
+  // check for an extra delimiter
+
+  var extraDelimiter = getExtraDelimiter(input);
+
+  // anlayzing string and build delimiters regexp
+
+  var delimiters = DEFAULT_DELIMITERS;
+
+  if (extraDelimiter) {
+    debug('detected an extra delimiter', extraDelimiter);
+
+    input = input.substring(3).trim();
+    debug('cleaning input from extra delimiters syntax', input);
+
+    delimiters.push(extraDelimiter);
+  }
+
+  debug('using delimiters', delimiters);
+
+  var DELIMITERS_REGEXP = new RegExp(delimiters.join('|'), 'g');
+
+  return input
+    .split(DELIMITERS_REGEXP)
+    .map(n => {
+      return parseInt(n);
+    });
+
+}
+
+
+/**
+ * Extract an optional extra delimiter from input string
+ * E.g.: for '//!\n1;2!3,1', '!' is the extra delimiter
+ *
+ * @param  {string} input
+ * @return {string|null}
+ */
+
+function getExtraDelimiter (input) {
+
+  var part = input.split('\n');
+  var delimiter = null;
+
+  if (part[0] && ~part[0].indexOf('//')) {
+    delimiter = part[0].replace('//', '');
+  }
+
+  return delimiter;
+
+}
